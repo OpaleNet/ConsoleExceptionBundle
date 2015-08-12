8fda3c67749d494b09556bdbad38459dc02bbc9e
Xavier RENAUDIN <xavier@opale.net>
2015-08-12 10:02:59 -0400

    create bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Opale\ConsoleExceptionBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('opale_console_exception');
+
+        return $treeBuilder;
+    }
+}
---
+++ OpaleConsoleExceptionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Opale\ConsoleExceptionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OpaleConsoleExceptionBundle extends Bundle
+{
+}
